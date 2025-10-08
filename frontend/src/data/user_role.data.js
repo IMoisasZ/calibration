1c9e70668dc876762f26725ac5c2a5b3386fe6f7
moisas <mopri08@gmail.com>
2025-10-07 23:58:32 -0300

    Create the data role user

---
+++ frontend/src/data/user_role.data.js
@@ -0,0 +1,5 @@
+export const USER_ROLES = [
+	{ role: 'MASTER', value: 'MASTER', label: 'Master' },
+	{ role: 'ADMINISTRADOR', value: 'ADMINISTRADOR', label: 'Administrador' },
+	{ role: 'USUARIO', value: 'USUARIO', label: 'Usuário' },
+]

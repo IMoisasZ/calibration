df7828a341069f6dd94fb3248109c4a5054328cf
moisas <mopri08@gmail.com>
2025-10-13 12:40:30 -0300

    Altered structure the server to receive prodution and development

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,60 @@
 /** @format */
 
-import app from './app.js'
-import dbConnection from './connection/db.connection.js'
-import { syncModels } from './models/__index.js'
-import { createUserDefault } from './utils/user.utils.js'
+/**@description -> Imports*/
 import dotenv from 'dotenv'
-dotenv.config()
+import path from 'node:path'
 
 const PORT = process.env.PORT || 3333
 
 try {
+	/**@description -> 1. LOAD THE ENVIRIOMENTAL VARIABLES */
+	const envFile =
+		process.env.NODE_ENV === 'production'
+			? '.env.production'
+			: '.env.development'
+	dotenv.config({ path: path.resolve(process.cwd(), envFile) })
+
+	/**@description -> 2. AFTER, IMPORT THE MODULES DEPENDENT */
+	/**@description -> Import and define the logger (winston), that depend of FILENAME and NODE_ENV. */
+	const loggerConfig = await import('./logger/logger_config.logger.js').then(
+		(m) => m.default
+	)
+	const { default: app } = await import('./app.js')
+	app.set('logger', loggerConfig)
+	global.logger = loggerConfig /**@description -> Define the logger global */
+
+	/**@description -> Import modules DB and utillyts, that depend of process.env */
+	const dbConnection = await import('./connection/db.connection.js').then(
+		(m) => m.default
+	)
+	const { syncModels } = await import('./models/__index.js')
+	const { createUserDefault } = await import('./utils/user.utils.js')
+
+	/**@description -> 3. INITIALIZATION OF APPLICATION */
+
 	/**@description -> Connection with database */
 	await dbConnection.authenticate()
-	console.warn('Connection has been established successfully.')
+	global.logger.info('Connection has been established successfully.') // Usando Winston
 
 	/**@description -> Sync the models with database */
 	await syncModels()
-	console.warn('The models are sync with database!')
-
-	/**@description -> Start the server */
-	app.listen(PORT, () => console.warn(`SERVER RUNNING ON PORT ${PORT}`))
+	global.logger.info('The models are sync with database!') // Usando Winston
 
 	/**@description -> Create the user default */
 	createUserDefault()
+
+	/**@description -> Start the server */
+	app.listen(PORT, () => global.logger.info(`SERVER RUNNING ON PORT ${PORT}`)) // Usando Winston
 } catch (error) {
-	console.error('Unable to connect to the database:', error)
-	process.exit(1)
+	/**@description -> 4. TRATAMENT OF CRITICAL ERROR */
+	if (global.logger) {
+		global.logger.error(
+			'Unable to connect to the database or start server:',
+			error
+		)
+	} else {
+		console.error('Unable to connect to the database or start server:', error)
+	}
+
+	process.exit(1) /**@description -> Finish the connection */
 }
